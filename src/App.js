9d84af19732e2a397d3e54bc2925cc7b81b93b72
Officialibn1 <officialibn001@gmail.com>
2023-01-07 22:33:29 +0100

    show func

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -27,12 +29,18 @@ function App() {
 
 // Add Task
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = { id, ...task}
+
+  setTasks([...tasks, newTask])
 }
 
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
+
+
 }
 
 // Toggle Reminder
@@ -48,7 +56,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
   );
