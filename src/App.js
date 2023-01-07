6c5d2761ba0c9244688e81cee5bc9c3cf801960e
Officialibn1 <officialibn001@gmail.com>
2023-01-07 22:40:23 +0100

    UI finished

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
