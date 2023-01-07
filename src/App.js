106e7b92043c3f85551a9feefe4da1409171dd7a
Officialibn1 <officialibn001@gmail.com>
2023-01-07 10:54:12 +0100

    reminder function

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
         id: 1,
         text: 'Doctors Appointment',
         day: 'Feb 5th at 2:30pm',
-        reminder: true
+        reminder: true,
     },
     {
         id: 2,
         text: 'Meeting at School',
         day: 'Feb 6th at 1:30pm',
-        reminder: true
+        reminder: true,
     },
     {
         id: 3,
         text: 'Food Shopping',
         day: 'Feb 7th at 2:30pm',
-        reminder: false
+        reminder: false,
     },
 ])
 
@@ -31,7 +31,12 @@ const deleteTask = (id) => {
 
 // Toggle Reminder
 const toggleReminder = (id) => {
-  console.log(id)
+  setTasks(
+    tasks.map((task) => 
+      task.id  === id ? { ...task, reminder: 
+      !task.reminder} : task
+    )
+  )
 }
 
   return (
