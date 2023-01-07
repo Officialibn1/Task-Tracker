bd029978dc4cc0028702e4f0adb2948b2dfd6304
Officialibn1 <officialibn001@gmail.com>
2023-01-07 05:04:09 +0100

    v

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>, 
+        </header>
+    );
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'blue',
+//     backgroundColor: 'grey',
+//     fontSize: '70px',
+//     fontStyle: 'italic'
+// }
+
+export default Header;
