8a3cbe99b757246a933f03ee3bb96cf1480346fe
Officialibn1 <officialibn001@gmail.com>
2023-01-07 20:57:26 +0100

    Addtask Func

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,28 @@
 import { useState } from "react";
 
-const AddTask = () => {
+const AddTask = ({onAdd}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please add task')
+            return
+        }
+
+        onAdd({ text, day, reminder})
+
+        setText('')
+        setDay('')
+        setReminder(false)
+
+    }
+
     return (
-        <form action="" className="add-task">
+        <form action="" className="add-task" onSubmit={onSubmit}>
             <div className="form-control">
                 <label>Task</label>
                 <input type="text" name="" id="" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)} />
@@ -17,7 +33,11 @@ const AddTask = () => {
             </div>
             <div className="form-control form-control-check">
                 <label>Set Reminder</label>
-                <input type="checkbox" name="" id="" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+                <input type="checkbox" checked={reminder} 
+                name="" id="" value={reminder} 
+                onChange={(e) => 
+                    setReminder(e.currentTarget.checked)} 
+                />
             </div>
 
             <input type="submit" value='Save Task' className="btn btn-block"/>
